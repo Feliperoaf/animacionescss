8862377bc3ac27a95a022d4c3192c84431d692d9
Felipe Roa <feliperoaf@gmail.com>
2024-05-21 00:07:39 -0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+var root = document.querySelector('#root')
+var toggler = document.querySelector('#menu-toggler')
+
+toggler.addEventListener('click', () => {
+root.classList.toggle('open')
+})
+
+var root = document.querySelector('#root')
+var toggler = document.querySelector('#menu-toggler_2')
+
+toggler.addEventListener('click', () => {
+root.classList.toggle('open')
+})
